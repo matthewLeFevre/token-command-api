6e2967c23c8222ce840dc322e8e8439e8d091575
Matthew LeFevre <matthewlefevre95@gmail.com>
2022-05-29 23:49:17 -0600

    🐛 FIX: Fix json token check comparison.

--- src/middleware/isValidJWT.ts
+++ src/middleware/isValidJWT.ts
@@ -6,13 +6,21 @@ import { RectifyOperator } from "rectifyjs";
  * Validate JWT
  */
 export default async function isValidJWT(req: Request, res, next) {
-    const token = req.body.token;
-    let tokenDetails;
-    try {
-        tokenDetails = await req.db.tables.tokens.getWithQuery("token", RectifyOperator.EQ, token);
-        next();
-    } catch (err:any) {
-        throw new CustomError("InvalidJWTError", err?.message || "Invalid JWT", 400);
-    }
-
+  const token = req.body.token;
+  let tokenDetails;
+  try {
+    const response = await req.db.tables.tokens.getWithQuery(
+      "token",
+      RectifyOperator.EQ,
+      token
+    );
+    tokenDetails = response._responses[0].r[0];
+    next();
+  } catch (err: any) {
+    throw new CustomError(
+      "InvalidJWTError",
+      err?.message || "Invalid JWT",
+      400
+    );
+  }
 }
